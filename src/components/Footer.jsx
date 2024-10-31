b0dfb58ee5498babb00cfd68f7a7373d114cdf13
maoly <maoly.bissemo@gmail.com>
2024-10-31 16:25:22 +0100

    css error fini

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <img src={logoBlanc} alt="logo kasa" className="footerLogo"/>
-            <p>© 2024 Kasa. All rights reserved</p>
+            <p>© 2024 Kasa. All <span>rights reserved</span> </p>
         </footer>
     );
 };
